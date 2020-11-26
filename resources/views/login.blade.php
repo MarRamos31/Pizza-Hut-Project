caf804bacb6bd151a2c66a6a6fe6cdafed96775c
MarRamos31 <71425847+MarRamos31@users.noreply.github.com>
2020-11-26 15:15:14 +0700

    initial commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css')}}">
+
+    <title>Flowelto Shop</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: red;">
+    <a class="navbar-brand" href="#">PHizza Hut</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle font-weight-bold" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Categories
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <a class="nav-link font-weight-bold" href="#">Login</a>
+            <a class="nav-link font-weight-bold" href="#">Register</a>
+
+        </div>
+    </div>
+</nav>
+
+
+</body>
+</html>
